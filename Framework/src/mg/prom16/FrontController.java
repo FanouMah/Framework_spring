3928ebeda65c7b884041944eaf079e72faad7ca4
Fanou <fmahafaliarimbola@gmail.com>
2024-05-10 08:50:45 +0300

    FanouMah

---
+++ Framework/src/mg/prom16/FrontController.java
@@ -0,0 +1,34 @@
+package mg.prom16;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class FrontController extends HttpServlet {
+
+    protected void processRequest(jakarta.servlet.http.HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println(request.getRequestURL().toString());
+            // or out.println(request.getRequestURI());
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+}
