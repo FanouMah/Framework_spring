e0e6493de9308cf21f73ec4cb1b3f388cce6aaa6
Fanou <fmahafaliarimbola@gmail.com>
2024-05-19 16:10:48 +0300

    Sprint 2

--- Framework/src/controller/Controller1.java
+++ Framework/src/controller/Controller1.java
@@ -1,10 +1,15 @@
 package controller;
 
-import Annotations.Controller;
+import Annotations.*;
 
 @Controller
 public class Controller1 {
-    public static void main(String[] args) {
-        
-    }
+
+    @Get(value = "/Controller1/method1")
+    public void method1() { }
+
+    @Get
+    public void method2() { }
+
+    public void method3() { }
 }
